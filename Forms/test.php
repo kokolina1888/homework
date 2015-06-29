c0a974d951a9757459486a79e0c1c143e13ceed3
Milena Tomova <kokolina1888@gmail.com>
2015-06-29 22:33:41 +0300

    COUNTERS?

---
+++ Forms/test.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+?>
+<html>
+    <head></head>
+    <body>
+        <form method="post" action="test.php">
+            <input type="submit" name="count" value="Start counting" />
+        </form>
+        <?php
+        if(isset($_POST['count'])){
+            if(!($_SESSION['count'])){
+                $_SESSION['count'] = 1;
+            }else{
+                $count = $_SESSION['count'] + 1;
+                $_SESSION['count'] = $count;
+            }
+        }
+        echo $_SESSION['count'];
+        ?>
+    </body>
+</html>
