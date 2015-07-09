e08ad1e6f2e4dc88ee41b99f912b12834f8c3e76
Milena Tomova <kokolina1888@gmail.com>
2015-07-09 17:04:30 +0300

    solved

--- php06 - mattrix/03_g.php
+++ php06 - mattrix/03_g.php
@@ -1,25 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>table</title>
+	<style type="text/css">
+	table {
+		border: 1px solid black;
+		border-collapse: collapse;
+	}
+	td {
+		border: 1px solid black;
+	}
+	</style>
+</head>
+<body>
 <?php 
-$m = 2;
+$m = 4;
+$num = $m;
+$row = 0;
+$col = -1;
+$flag = 1;
+$var = 0;
 $arr = array(array());
-$j = 0;
-$num = 0;
-for ($i=0 ; $i < $m ; $i++ ) { 
-	$num++;
-	$arr[$j][$i] = $num;
-	
-}
-$j++;
-$num = $num + $m;
-for ($i=$m-1; $i >=0 ; $i--) { 
-	$arr[$j][$i] = $num;
+while ($num > 0) {
+	/*1
+	$num = 4;
+	$row = 0;
+	$col = -1;
+	$flag = 1;
+	$var = 0;
+	*/
+	/*
+	$num = 2;
+	$row = 1;
+	$col = 0;
+	$flag = 1;
+	$var = 12;
+	*/
+
+	while ($flag <= $num) {
+		$var++;
+		$col++;
+		$arr[$row][$col] = $var;		
+		$flag++;
+
+	}
 	$num--;
-}
-var_dump($arr);
-echo "<br />";
-for ($l=0; $l < $m; $l++) { 
-	foreach ($arr[$l] as $value) {
-		echo $value.' ';
+	$flag = 1;
+	/*3
+	$num = 3;
+	$row = 0;
+	$col = 3;
+	$flag = 1;
+	$var = 4;
+	*/
+	/*
+	$num = 1;
+	$row = 1;
+	$col = 2;
+	$flag = 1;
+	$var = 14;
+	*/
+
+while ($flag <= $num) {
+		$var++;
+		$row++;
+		$arr[$row][$col] = $var;
+		$flag++;
+		
 	}
 	echo "<br />";
+	$flag = 1;
+	//3
+	/*
+	$num = 3;
+	$row = 3;
+	$col = 3;
+	$flag = 1;
+	$var = 7;
+	*/
+	/*
+	$num = 1;
+	$row = 2;
+	$col = 2;
+	$flag = 1;
+	$var = 15;
+	*/
+
+	while ($flag <= $num) {
+		$var++;
+		$col--;
+		$arr[$row][$col] = $var;
+		$flag++;
+		}
+		$num--;
+		$flag = 1;
+	//4
+		/*
+	$num = 2;
+	$row = 2;
+	$col = 1;
+	$flag = 1;
+	$var = 16;
+	*/
+	/*
+	$num = 0;
+	$row = 1;
+	$col = 0;
+	$flag = 1;
+	$var = 12;
+	*/
+
+	while ($flag <= $num) {
+			$var++;
+			$row--;
+			$arr[$row][$col] = $var;
+			$flag++;
+		}	
+		$flag = 1;
+		/*
+	$num = 2;
+	$row = 1;
+	$col = 0;
+	$flag = 1;
+	$var = 12;
+	*/
+
 }
 ?>
+<table>
+<?php
+for ($i=0; $i < $m ; $i++) { 
+	echo "<tr>";
+	for ($j=0; $j < $m; $j++) { 
+		echo '<td>';
+		//echo "[$i,$j] - ";
+		echo $arr[$i][$j];
+		echo '<td />';	
+	}
+	echo "<tr />";
+}
+?>
+<table/>
+</body>
+</html>
